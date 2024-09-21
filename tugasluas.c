d7c0cde6c0ab59ede3b8bd75a28f9e4a2fd61c97
galangkunn <112243284+galangkunn@users.noreply.github.com>
2024-09-21 15:50:52 +0700

    Add files via upload

---
+++ tugasluas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    //disini untuk mendeklarasikan panjang lebar keliling dan luas persegi panjang
+    int p,l,k,lu;
+    p = 10;
+    l = 7;
+    k = (p+l)*2;
+    lu = p*l;
+    //Disini Untuk menampilkan Panjang Lebar dan luasnya
+    printf("Panjang Adalah %d cm \n",p);
+    printf("Lebar Adalah %d cm \n",l);
+    printf("Keliling Persegi Panjang Tersebut Adalah %d cm \n",k);
+    printf("Luas Adalah %d cm2",lu);
+    return 0;
+}
---
+++ tugasnamanim.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("2441015 \n");
+    printf("Galang Putra Pratama \n");
+    printf("Teknik INformatika \n");
+    return 0;
+}
